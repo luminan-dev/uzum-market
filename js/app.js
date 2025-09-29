87d9ac8e89776beecfecf2a9a85565d64159cfd6
luminan-dev <pardaevasadbek02@gmail.com>
2025-09-29 06:42:43 +0500

    second commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// js/app.js
+document.addEventListener('DOMContentLoaded', () => {
+  const heroSwiper = new Swiper('.Hero-swiper', {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
